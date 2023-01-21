62209d87d6d4af0333975cf9ef25e56593416c04
jhoward14ST <86802470+jhoward14ST@users.noreply.github.com>
2023-01-21 14:57:49 -0800

    Commit for changes made in raz4

--- Program.cs
+++ Program.cs
@@ -43,7 +43,8 @@ namespace ContosoUniversity
                 var services = scope.ServiceProvider;
 
                 var context = services.GetRequiredService<SchoolContext>();
-                context.Database.EnsureCreated();
+                // howarj9 - raz4
+                //context.Database.EnsureCreated();
                 // howarj9 - raz1
                 DbInitializer.Initialize(context);
             }
